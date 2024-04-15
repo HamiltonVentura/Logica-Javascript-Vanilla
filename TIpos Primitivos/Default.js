ca488807efca1007ac9ffe4298dbca5b0618e631
Hamilton Rodrigues <hamiltonventtura@gmail.com>
2024-04-15 16:48:47 -0300

    dados

---
+++ TIpos Primitivos/Default.js
@@ -0,0 +1,15 @@
+let tipoString = String("68273684237");
+console.log("A string é : ", tipoString);
+
+let dados = "45";
+let tipoNUmber = Number(dados);
+console.log("Convertido em numero  :  " + tipoNUmber);
+
+let bol = false;
+let bol2 = "false";
+
+console.log(Boolean(bol));
+console.log(Boolean(bol2));
+
+console.log(2 === "2");
+console.log("2" === "2");
